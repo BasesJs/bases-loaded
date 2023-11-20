e9a6822f813fffe3570fc3723f87349145411067
Jacob Dahlke <jacob.dahlke@npsols.com>
2023-11-20 11:09:53 -0500

    test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ LoadBases()
         console.log("Bases Loaded");
     })
     .then(()=>{
-        bases.core.filetypes.get("systemName", "Image File Format")
+        bases.core.filetypes.get("id", ["39","16","74"])
         .then(items => {
             console.log(items);
         })
